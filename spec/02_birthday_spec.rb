e0b875cc9c7afef917c418585e4126377bf4bf4d
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-13 23:03:15 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -7,9 +7,7 @@ describe "#happy_birthday" do
       "Sarah" => 6,
       "Amanda" => 27
     }
-
     expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
-
   end
 end
 
